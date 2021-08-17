01f12bc50d0e4a53513406dd5025c0b9755c3060
Tomofumi Oga <nuko973663@gmail.com>
2021-08-17 12:04:34 +0900

    miner bug fix

--- src/jpycpayqr.js
+++ src/jpycpayqr.js
@@ -151,6 +151,7 @@ $(() => {
               elm["amount"] = amount;
               $("#toastContainer").prepend(build_toast(toast_text, elm));
               $(".toast").toast("show");
+              move_to_link($("#toast" + elm["hash"]));
             }
             let blockNum = parseInt(elm["blockNumber"]);
             if (blockNum > last_block) {
@@ -221,11 +222,11 @@ $(() => {
   });
 });
 
-var toast_text = `<div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
+var toast_text = `<div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false" id="toast#hash#">
   <div class="toast-header">
     <svg class="bd-placeholder-img rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" ><rect fill="#007aff" width="100%" height="100%"/></svg>
     <strong class="mr-auto">#amount# #tokenSymbol#</strong>
-    <small class="text-muted">#time#</small>
+    <small class="text-muted">just now</small>
     <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
       <span aria-hidden="true">&times;</span>
     </button>
